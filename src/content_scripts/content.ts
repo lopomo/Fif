77e0771ef7f7937c407b681d86faaddc4c119ea5
Vitez, Radko <Radko.Vitez@2ring.com>
2017-09-04 10:42:45 +0200

    only for panel_teamassignment

--- src/content_scripts/content.ts
+++ src/content_scripts/content.ts
@@ -1,11 +1,14 @@
 ﻿module BrowserExtension {
 	export class ContentScript {
 		public initialize = () => {
-			let iframes = document.getElementsByTagName("iframe");
-			for (let index = 0; index < iframes.length; index++) {
-				let element = iframes[index];
-				element.style.height = "1000px";
-				
+			let mainPanel = document.getElementById("panel_teamassignment");
+			if (mainPanel) {
+				let iframes = mainPanel.getElementsByTagName("iframe");
+				for (let index = 0; index < iframes.length; index++) {
+					let element = iframes[index];
+					element.style.height = "1000px";
+					
+				}
 			}
 		}
 	}
