5341ea9a8794368f82f3bcccca57c0d3b6947a88
Vitez, Radko <Radko.Vitez@2ring.com>
2018-10-11 10:41:18 +0200

    fix

--- src/content_scripts/removeDoneColumn.ts
+++ src/content_scripts/removeDoneColumn.ts
@@ -3,21 +3,25 @@ namespace Fif {
 		public initialize = () => {
             const lastcolumns = document.querySelectorAll('td.taskboard-cell.ui-droppable[axis="taskboard-table-body_s5"]');
             for(let i = 0; i < lastcolumns.length; i++) {
-                lastcolumns[i].remove();
+                (lastcolumns[i] as HTMLElement).style.display = 'none';;
             }
-            document.getElementById('taskboard-table-body').style['table-layout'] = 'auto';
-            document.getElementById('taskboard-table-header').style['table-layout'] = 'auto';
-            if (document.getElementById('taskboard-table-header_s5')) {
-                document.getElementById('taskboard-table-header_s5').remove();
-            }
-            const others = document.querySelectorAll('td.taskboard-cell.ui-droppable[axis*="taskboard-table-body_s"]');
-            for(let i = 0; i < others.length; i++) {
-                others[i].setAttribute("style", "width: 20%;");
-            }
-            const headers = document.querySelectorAll('th.taskboard-cell[id*="taskboard-table-header_s"]');
-            for(let i = 0; i < headers.length; i++) {
-                headers[i].setAttribute("style", "width: 20%;");
-            }
+            // if (document.getElementById('taskboard-table-body')) {
+            //     document.getElementById('taskboard-table-body').style['table-layout'] = 'auto';
+            // }
+            // if (document.getElementById('taskboard-table-header')) {
+            //     document.getElementById('taskboard-table-header').style['table-layout'] = 'auto';
+            // }
+            // if (document.getElementById('taskboard-table-header_s5')) {
+            //     document.getElementById('taskboard-table-header_s5').style.display = 'none';
+            // }
+            // const others = document.querySelectorAll('td.taskboard-cell.ui-droppable[axis*="taskboard-table-body_s"]');
+            // for(let i = 0; i < others.length; i++) {
+            //     others[i].setAttribute("style", "width: 20%;");
+            // }
+            // const headers = document.querySelectorAll('th.taskboard-cell[id*="taskboard-table-header_s"]');
+            // for(let i = 0; i < headers.length; i++) {
+            //     headers[i].setAttribute("style", "width: 20%;");
+            // }
 
             const targetNode = document.getElementById('PopupContentContainer');
             const config = { attributes: false, childList: true, subtree: false };
